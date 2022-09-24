613f83eb743fcdde2f12a5c5394391f101e4c3c2
Artem Trushkin <at.120@ya.ru>
2022-09-24 17:26:12 +0700

    Png exporter cleaned up

--- Landscape generation/ImageExporter/PngExporter/PngExporter.cs
+++ Landscape generation/ImageExporter/PngExporter/PngExporter.cs
@@ -1,14 +1,10 @@
-using System.Drawing;
-using System.Drawing.Imaging;
-
 class PngExporter : ImageExporter
 {
     void ImageExporter.Export(Map map, string filename)
     {
         FileStream file = new FileStream(filename, FileMode.Create, FileAccess.Write);
-
-        List<byte> rawData = new List<byte>();
-        int height = map.cells.GetLength(0); //TODO: x или y сначала?
+        
+        int height = map.cells.GetLength(0);
         int width = map.cells.GetLength(1);
         
         PngHeader header = new PngHeader();
